509ba44c5c73aea843be4876dc0ebae717e999ca
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-05-27 01:07:25 +0530

    finished

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,18 @@
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import {uiActions} from '../../store/ui-slice';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-  const cartQuantity = useSelector((state) => state.cart.totalQuantity)
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
-  }
+  };
+
   return (
-    <button className={classes.button} onClick= {toggleCartHandler}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>{cartQuantity}</span>
     </button>
